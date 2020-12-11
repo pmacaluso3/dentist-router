c1611b49a68dd6386a87cf4e111d9680b9e00af2
famousPete <pete.macaluso@generalassemb.ly>
2020-12-11 11:11:56 -0500

    Routes work but you have to type them in manually

---
+++ src/components/Contact.js
@@ -0,0 +1,5 @@
+function Contact() {
+  return(<div>This is the Contact Page</div>)
+}
+
+export default Contact
---
+++ src/components/Procedures.js
@@ -0,0 +1,5 @@
+function Procedures() {
+  return(<div>This is the Procedures Page</div>)
+}
+
+export default Procedures
